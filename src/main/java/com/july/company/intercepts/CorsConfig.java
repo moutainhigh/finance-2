8df8185ed75dd00adb341f364771c27d9db29b25
zengxueqi-yu@outlook.com <zengxueqi-yu@outlook.com>
2020-05-21 18:26:42 +0800

    解决跨域问题

---
+++ src/main/java/com/july/company/intercepts/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.july.company.intercepts;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 跨域配置
+ * @author zengxueqi
+ * @program finance
+ * @since 2020-05-21 18:24
+ **/
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .maxAge(3600);
+    }
+
+}
---
+++ src/main/java/com/july/company/intercepts/CorsFilter.java
@@ -0,0 +1,33 @@
+package com.july.company.intercepts;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 跨域信息
+ * @author zengxueqi
+ * @program finance
+ * @since 2020-05-21 18:23
+ **/
+@Component
+public class CorsFilter {
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, HEAD");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "access-control-allow-origin, authority, content-type, version-info, X-Requested-With");
+        chain.doFilter(req, res);
+    }
+
+    public void init(FilterConfig filterConfig) {
+    }
+
+    public void destroy() {
+    }
+
+}
