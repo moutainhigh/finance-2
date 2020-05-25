bfc8f62ea0749b21ae5e30faea9c7a2c9a499175
周均 <87538812@qq.com>
2020-05-25 20:30:51 +0800

    标题组件

---
+++ node/src/components/DespComp.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="guquan-title">
+        <div>{{title}}</div>
+        <div>{{enTitle}}</div>
+    </div>
+</template>
+<script>
+export default {
+    name:'DespComp',
+    data(){
+        return {
+            title:'债券融资',
+            enTitle:'Bond FINANCING'
+        }
+    },
+    created() {
+        if(this.$route.path=='/guquan'){
+            this.title="股权融资";
+            this.entitle="EQUITY FINANCING";
+        }
+    },
+    methods: {
+        
+    },
+}
+</script>
+<<style lang="scss" scope>
+.guquan-title{position:absolute;top:136px;left:130px;}
+.guquan-title div:nth-child(1){font-family: BDZYJT--GB1-0;font-size: 42px;color: #FFFFFF;}
+.guquan-title div:nth-child(2){font-family: PingFangSC-Regular;font-size: 20px;color: #FFFFFF;}
+</style>
